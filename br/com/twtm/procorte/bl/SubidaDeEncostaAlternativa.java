5b54ec5f23d56cf0afe0527cb7a724790581f4dd
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-07-24 19:08:32 -0300

    Adição do Subida de Encosta Alternativa

---
+++ br/com/twtm/procorte/bl/SubidaDeEncostaAlternativa.java
@@ -0,0 +1,49 @@
+package br.com.twtm.procorte.bl;
+
+public class SubidaDeEncostaAlternativa {
+
+    public static void main(String args[]) {
+
+        int p[][] = new int[3][3];
+        int qtp = p.length;
+        int d1mp = 10;
+        int d2mp = 10;
+        int mp[][] = new int[d1mp][d2mp];
+
+        p[0][0] = 2; //altura
+        p[0][1] = 2; //largura
+        p[0][2] = 3; //quantidade
+        p[1][0] = 3;
+        p[1][1] = 3;
+        p[1][2] = 5;
+        p[2][0] = 4;
+        p[2][1] = 4;
+        p[2][2] = 1;
+
+        int tmax = 4;
+        int t = 0;
+
+        Procorte pc = new Procorte(p, mp);
+        int atual[][] = pc.geraSolucaoInicial();
+        int va = pc.avalia(atual);
+        int proximo[][];
+        int vp;
+        while (true) {
+            proximo = pc.geraSucessores();
+            vp = pc.avalia(proximo);
+            if (vp >= va) {
+                if (t <= tmax) {
+                    t++;
+                } else {
+                    break;
+                }
+            } else {
+                atual = proximo;
+                va = vp;
+                t = 0;
+            }
+        }
+
+    }
+
+}
